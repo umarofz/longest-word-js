ccf3712631e65f9153696fcc3bc4fe60e3b8f4cf
umarofz <webnajottalim@gmail.com>
2022-07-17 22:56:37 +0300

    Add

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var elForm = document.querySelector(".form")
+var elInput = document.querySelector(".input")
+
+elForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+
+    var inputValue = elInput.value.trim()
+
+    
+    function findWord(text, number) {
+        var newArray = text.split(" ");
+        var longestWord = "";
+
+        var resultArray = []
+
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i].length > longestWord.length) {
+                longestWord = newArray[i];
+            }
+        }
+
+        return longestWord
+
+    }
+
+    var arrayOfWords = findWord(inputValue)
+
+    console.log(arrayOfWords);
+})
